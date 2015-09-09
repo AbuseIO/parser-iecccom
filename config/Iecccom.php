6bad1a672dd031a996b109683cd1b2b1f0d598bb
Bart Vrancken <bart@abuse.io>
2015-09-09 21:40:23 +0200

    migrate iecccom parser from 3.1 to 4.0

---
+++ config/Iecccom.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'parser' => [
+        'name'          => 'IECCCOM',
+        'enabled'       => true,
+        'sender_map'    => [
+            '/@r.iecc.com/',
+        ],
+        'body_map'      => [
+            //
+        ],
+    ],
+
+    'feeds' => [
+        'default' => [
+            'class'     => 'SPAM',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                'Source-IP',
+                'Feedback-Type',
+                'Received-Date',
+            ],
+        ],
+
+    ],
+];
