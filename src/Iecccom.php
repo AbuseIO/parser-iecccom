199140fbe73ae4fb32ab39825d7dcbef84923517
Bart Vrancken <bart@abuse.io>
2015-12-30 12:43:39 +0100

    add docblocks

--- src/Iecccom.php
+++ src/Iecccom.php
@@ -2,10 +2,17 @@
 
 namespace AbuseIO\Parsers;
 
+/**
+ * Class Iecccom
+ * @package AbuseIO\Parsers
+ */
 class Iecccom extends Parser
 {
     /**
      * Create a new Blocklistde instance
+     *
+     * @param \PhpMimeMailParser\Parser $parsedMail phpMimeParser object
+     * @param array $arfMail array with ARF detected results
      */
     public function __construct($parsedMail, $arfMail)
     {
@@ -14,7 +21,7 @@ class Iecccom extends Parser
 
     /**
      * Parse attachments
-     * @return Array    Returns array with failed or success data
+     * @return array    Returns array with failed or success data
      *                  (See parser-common/src/Parser.php) for more info.
      */
     public function parse()
